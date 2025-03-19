dc8e2dbcb234e0f566a6b378814cc5b7dacb3f42
carlosjac312 <115487243+carlosjac312@users.noreply.github.com>
2025-03-19 10:40:28 +0100

    Add files via upload

---
+++ practica2/routes/dicctionary/[word].tsx
@@ -0,0 +1,76 @@
+import { FreshContext, Handlers, PageProps } from "$fresh/server.ts";
+import Axios from "npm:axios";
+import Meanings from "../../components/meanings.tsx";
+import Phonetics from "../../components/phonetics.tsx";
+
+type definiciones = {
+  definition: string;
+  example?: string;
+  synonyms: string[];
+  antonyms: string[];
+};
+
+type significado = {
+  partOfSpeech: string;
+  definitions: definiciones[];
+};
+
+type fonetico = {
+  text?: string;
+  audio: string;
+  sourceUrl?: string;
+  license?: {
+    name: string;
+    url: string;
+  };
+};
+
+type DictionaryEntry = {
+  word: string;
+  phonetics: fonetico[];
+  meanings: significado[];
+};
+
+type DictionaryResponse = DictionaryEntry[];
+
+type Props = {
+  palabra:string
+  phonetics: fonetico[];
+  meanings: significado[];
+};
+
+export const handler: Handlers = {
+  GET: async (req: Request, ctx: FreshContext<unknown, Props>) => {
+    const {word} = ctx.params;
+    try {
+      const infoWord = await Axios.get<DictionaryResponse>(
+        `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`,
+      );
+      const fonetico = infoWord.data[0].phonetics;
+      const significado = infoWord.data[0].meanings;
+      return ctx.render({
+        palabra:word,
+        phonetics: fonetico,
+        meanings: significado,
+      });
+    } catch (error) {
+      throw new Error(
+        "The connection with API went wrong or you put a non-existent word",
+      );
+    }
+  },
+};
+
+const Page = (props: PageProps<Props>) => {
+  return (
+    <div>
+      <h1 style={{ paddingLeft: "20px" }}>{props.data.palabra}</h1>
+      <div class="container">
+        <Phonetics foneticos={props.data.phonetics} />
+        <Meanings significados={props.data.meanings} />
+      </div>
+    </div>
+  );
+};
+
+export default Page;
